d67ca8eebe4f212d4330cdf3fa275e1878389685
SantiagoJSdev <santiagoreyesmarin1986@gmail.com>
2021-12-27 16:12:15 -0500

    commit

--- src/components/BotonCalc.tsx
+++ src/components/BotonCalc.tsx
@@ -1,21 +1,32 @@
 import React from 'react'
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { style } from '../theme/appTheme';
 
 interface props {
 texto: string;
 //aqui coloco signo de interrogacion indicando q es opcional q se envie
 color?: string;
+ancho?: boolean;
 }
 
-export const BotonCalc = ({texto, color='2d2d2d'}:props) => {
+export const BotonCalc = ({texto, color = '#2D2D2D', ancho= false}:props) => {
     return (
-        <View style={{
-             ...style.boton,
-             backgroundColor: color
-            
-            }}>
-             <Text style={style.botonTexto}>{texto}</Text>
-        </View>
+
+        <TouchableOpacity>
+            <View style={{
+                ...style.boton,
+                backgroundColor: color,
+                width: (ancho) ? 180 : 80
+                
+                
+                }}>
+                <Text style={{
+                    ...style.botonTexto,
+                    color:(color === '#9b9b9b') ? 'black' : 'white'
+                    }}
+                    
+                    >{texto}</Text>
+            </View>
+        </TouchableOpacity>
     )
 }
--- src/screens/CalculadoraScreen.tsx
+++ src/screens/CalculadoraScreen.tsx
@@ -9,18 +9,42 @@ export const CalculadoraScreen = () => {
             <Text style={style.resultadoPequeño}>1,500.00</Text>
             <Text style={style.resultado}>1,500.00</Text>
 
-
+{/* ////////////////fila de botones ////////////// */}
             <View style={style.fila}>
-
                 <BotonCalc texto='C' color='#9b9b9b'/>
                 <BotonCalc texto='+/-' color='#9b9b9b'/>
                 <BotonCalc texto='del' color='#9b9b9b'/>
                 <BotonCalc texto='/' color='#ff9427'/>
+            </View>
+            {/* ////////////////fila de botones ////////////// */}
 
-            
-
+            <View style={style.fila}>
+                <BotonCalc texto='7'   />
+                <BotonCalc texto='8' />
+                <BotonCalc texto='9' />
+                <BotonCalc texto='*' color='#ff9427'/>
             </View>
 
+            <View style={style.fila}>
+                <BotonCalc texto='4'   />
+                <BotonCalc texto='5' />
+                <BotonCalc texto='6' />
+                <BotonCalc texto='-' color='#ff9427'/>
+            </View>
+
+            <View style={style.fila}>
+                <BotonCalc texto='1'   />
+                <BotonCalc texto='2' />
+                <BotonCalc texto='3' />
+                <BotonCalc texto='+' color='#ff9427'/>
+            </View>
+
+            <View style={style.fila}>
+                <BotonCalc texto='0' ancho  />
+           
+                <BotonCalc texto='.' />
+                <BotonCalc texto='=' color='#ff9427'/>
+            </View>
 
         </View>
     )
